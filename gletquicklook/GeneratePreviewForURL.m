c675f0e4b2b2520471999c4d11e4fe3ee3973717
Matt Swain <matt@macosxtips.co.uk>
2011-04-17 04:40:57 +0100

    Added in embedded glet icon, finished off CSS styling.

--- gletquicklook/GeneratePreviewForURL.m
+++ gletquicklook/GeneratePreviewForURL.m
@@ -12,15 +12,20 @@
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    CFBundleRef bundle = QLPreviewRequestGetGeneratorBundle(preview);
+    CFURLRef iconUrl = CFBundleCopyResourceURL(bundle, CFSTR("icon.png"), NULL, NULL);
+    NSString *iconUrlString = [(NSURL *)iconUrl path];
 	NSDictionary *fileDict = [NSDictionary dictionaryWithContentsOfFile:[(NSURL *)url path]];
 	NSString *name = [fileDict objectForKey:@"Name"];
-	NSString *command = [(NSString *) CFXMLCreateStringByEscapingEntities(NULL, (CFStringRef) [fileDict objectForKey:@"Command"], NULL) autorelease];
+	NSString *command = [(NSString *)CFXMLCreateStringByEscapingEntities(NULL, (CFStringRef) [fileDict objectForKey:@"Command"], NULL) autorelease];
 	NSString *font = [fileDict objectForKey:@"FontFamily"];
 	NSNumber *fontSize = [fileDict objectForKey:@"FontSize"];
 	NSString *coords = [fileDict objectForKey:@"GeekletFrame"];
 	NSNumber *refresh = [fileDict objectForKey:@"RefreshInterval"];
 	NSMutableString *html = [[[NSMutableString alloc] init] autorelease];
-	[html appendString:@"<html><head><style>body{background:#f1f1f1} dl{font:14px 'Lucida Grande';margin-left:140px;} dt{clear:left;float:left;margin:0 0 0 -130px;padding:5px 0;font-weight:bold;color:#808080;text-align:right;width:120px} dd{margin:0;padding:5px;} pre{margin: 0px;white-space:pre-wrap;word-wrap:break-word;}</style><body><dl>"];
+	[html appendString:@"<html><head><style>body{font:14px 'Lucida Grande';background: -webkit-gradient(linear, left top, left bottom, from(#333), to(#222));margin:15px;} dl{padding: 10px 5px 10px 140px;border-radius:6px;background: -webkit-gradient(linear, left top, left bottom, from(#f1f1f1), to(#d1d1d1));-webkit-box-shadow:inset 0 1px 3px #fff, 0 0 20px #000;} dt{clear:left;float:left;margin:0 0 0 -130px;padding:5px 0;font-weight:bold;color:#808080;text-align:right;width:120px;text-shadow: #fff 0px 1px 0px;} dd{margin:0;padding:5px;} pre{margin: 0px;white-space:pre-wrap;word-wrap:break-word;} h1{display:inline-block;margin: 50px 0 0;vertical-align:top;color:#dddddd}</style><body><img src='cid:icon.png' /><h1>"];
+    [html appendString:[(NSURL *)url lastPathComponent]];
+    [html appendString:@"</h1><dl>"];
 	if (name) {
 		[html appendString:@"<dt>Name</dt><dd>"];
 		[html appendString:name];
@@ -46,10 +51,16 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
         [html appendString:[refresh stringValue]];
     }
     [html appendString:@"</dd></dl></body></html>"];
-	
-	CFDictionaryRef props = (CFDictionaryRef) [NSDictionary dictionary];
-	QLPreviewRequestSetDataRepresentation(preview, (CFDataRef)[html dataUsingEncoding:NSUTF8StringEncoding], kUTTypeHTML, props);
-	
+    NSLog(@"%@", html);
+    NSMutableDictionary *props = [[[NSMutableDictionary alloc] init] autorelease];
+    [props setObject:@"UTF-8" forKey:(NSString *)kQLPreviewPropertyTextEncodingNameKey];
+    [props setObject:@"text/html" forKey:(NSString *)kQLPreviewPropertyMIMETypeKey];
+    NSData *image=[NSData dataWithContentsOfFile:iconUrlString];
+    NSMutableDictionary *imgProps=[[[NSMutableDictionary alloc] init] autorelease];
+    [imgProps setObject:@"image/png" forKey:(NSString *)kQLPreviewPropertyMIMETypeKey];
+    [imgProps setObject:image forKey:(NSString *)kQLPreviewPropertyAttachmentDataKey];
+    [props setObject:[NSDictionary dictionaryWithObject:imgProps forKey:@"icon.png"] forKey:(NSString *)kQLPreviewPropertyAttachmentsKey];
+	QLPreviewRequestSetDataRepresentation(preview, (CFDataRef)[html dataUsingEncoding:NSUTF8StringEncoding], kUTTypeHTML, (CFDictionaryRef)props);
 	[pool release];
 	return noErr;
 }
