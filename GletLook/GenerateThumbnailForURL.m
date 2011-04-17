fabe83c57e1a5219618e319c3407a96690d19121
Matt Swain <matt@macosxtips.co.uk>
2011-04-17 15:20:38 +0100

    Added html template file to avoid dynamically generating html and css

--- GletLook/GenerateThumbnailForURL.m
+++ GletLook/GenerateThumbnailForURL.m
@@ -10,6 +10,7 @@
 
 OSStatus GenerateThumbnailForURL(void *thisInterface, QLThumbnailRequestRef thumbnail, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options, CGSize maxSize)
 {
+    // Not worth implementing for .glet files
     return noErr;
 }
 
