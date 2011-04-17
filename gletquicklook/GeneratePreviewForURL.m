b05d391f90d55d0aeff5a24d6ad6c6a6860578a3
Matt Swain <matt@macosxtips.co.uk>
2011-04-17 02:53:18 +0100

    Styled CSS in preview, accounted for possibility of missing keys

--- gletquicklook/GeneratePreviewForURL.m
+++ gletquicklook/GeneratePreviewForURL.m
@@ -12,44 +12,49 @@
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-	NSURL *nsUrl = (NSURL *)url;
-    
-    NSDictionary *fileDict = [NSDictionary dictionaryWithContentsOfFile:[nsUrl path]];
-    NSString *name = [fileDict objectForKey:@"Name"];
-    NSString *command = [fileDict objectForKey:@"Command"];
-    NSString *escapedCommand = [(NSString *) CFXMLCreateStringByEscapingEntities(NULL, (CFStringRef) command, NULL) autorelease];
-    NSString *font = [fileDict objectForKey:@"FontFamily"];
-    NSNumber *fontSize = [fileDict objectForKey:@"FontSize"];
-    NSString *coords = [fileDict objectForKey:@"GeekletFrame"];
-    NSNumber *refresh = [fileDict objectForKey:@"RefreshInterval"];
-    
+	NSDictionary *fileDict = [NSDictionary dictionaryWithContentsOfFile:[(NSURL *)url path]];
+	NSString *name = [fileDict objectForKey:@"Name"];
+	NSString *command = [(NSString *) CFXMLCreateStringByEscapingEntities(NULL, (CFStringRef) [fileDict objectForKey:@"Command"], NULL) autorelease];
+	NSString *font = [fileDict objectForKey:@"FontFamily"];
+	NSNumber *fontSize = [fileDict objectForKey:@"FontSize"];
+	NSString *coords = [fileDict objectForKey:@"GeekletFrame"];
+	NSNumber *refresh = [fileDict objectForKey:@"RefreshInterval"];
 	NSMutableString *html = [[[NSMutableString alloc] init] autorelease];
-	[html appendString:@"<html><head><style>dl{float:left;width:520px;margin:1em 0;padding:0;} dt{clear:left;float:left;width:200px;margin:0;padding:5px;font-weight:bold;} dd{float:left;width:300px;margin:0;padding:5px;}</style><body><dl><dt>Name</dt><dd>"];
-    [html appendString:name];
-    [html appendString:@"</dd><dt>Command</dt><dd>"];
-    [html appendString:escapedCommand];
-    [html appendString:@"</dd><dt>Font</dt><dd>"];
-    [html appendString:font];
-    [html appendString:@"</dd><dt>Font Size</dt><dd>"];
-    [html appendString:[fontSize stringValue]];
-    [html appendString:@"</dd><dt>Coordinates</dt><dd>"];
-    [html appendString:coords];
-    [html appendString:@"</dd><dt>Refresh Rate</dt><dd>"];
-    [html appendString:[refresh stringValue]];
+	[html appendString:@"<html><head><style>body{background:#f1f1f1} dl{font:14px 'Lucida Grande';margin-left:140px;} dt{clear:left;float:left;margin:0 0 0 -130px;padding:5px 0;font-weight:bold;color:#808080;text-align:right;width:120px} dd{margin:0;padding:5px;} pre{margin: 0px;white-space:pre-wrap;word-wrap:break-word;}</style><body><dl>"];
+	if (name) {
+		[html appendString:@"<dt>Name</dt><dd>"];
+		[html appendString:name];
+	}
+	if (command) {
+		[html appendString:@"</dd><dt>Command</dt><dd><pre>"];
+		[html appendString:command];
+	}
+	if (font) {
+        [html appendString:@"</pre></dd><dt>Font</dt><dd>"];
+        [html appendString:font];
+    }
+	if (fontSize) {
+        [html appendString:@"</dd><dt>Font Size</dt><dd>"];
+        [html appendString:[fontSize stringValue]];
+    }
+	if (coords) {
+        [html appendString:@"</dd><dt>Coordinates</dt><dd>"];
+        [html appendString:coords];
+    }
+	if (refresh) {
+        [html appendString:@"</dd><dt>Refresh Rate</dt><dd>"];
+        [html appendString:[refresh stringValue]];
+    }
     [html appendString:@"</dd></dl></body></html>"];
-    
+	
 	CFDictionaryRef props = (CFDictionaryRef) [NSDictionary dictionary];
-	QLPreviewRequestSetDataRepresentation(
-                                          preview,
-                                          (CFDataRef)[html dataUsingEncoding:NSUTF8StringEncoding],
-                                          kUTTypeHTML, 
-                                          props);
-    
+	QLPreviewRequestSetDataRepresentation(preview, (CFDataRef)[html dataUsingEncoding:NSUTF8StringEncoding], kUTTypeHTML, props);
+	
 	[pool release];
 	return noErr;
 }
 
 void CancelPreviewGeneration(void* thisInterface, QLPreviewRequestRef preview)
 {
-    // implement only if supported
+	// implement only if supported
 }
